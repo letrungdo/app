46675ec835d779a2cb1b9ac03ae14fd2a30405c5
DoLT <dolt@guide.inc>
2023-06-30 12:19:33 +0700

    feat: add pdf api no-cors

--- next.config.js
+++ next.config.js
@@ -14,18 +14,18 @@ module.exports = {
             },
         ];
     },
-    // async headers() {
-    //     return [
-    //         {
-    //             // matching all API routes
-    //             source: "/api/:path*",
-    //             headers: [
-    //                 { key: "Access-Control-Allow-Credentials", value: "true" },
-    //                 { key: "Access-Control-Allow-Origin", value: "*" },
-    //                 { key: "Access-Control-Allow-Methods", value: "DELETE, POST, GET, OPTIONS" },
-    //                 { key: "Access-Control-Allow-Headers", value: "*" },
-    //             ],
-    //         },
-    //     ];
-    // },
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "DELETE, POST, GET, OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "*" },
+                ],
+            },
+        ];
+    },
 };
